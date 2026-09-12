f22d207a236909c229c5c8a3c51d9ef76500a6fe
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-12 12:42:56 -0400

    Adiciona detecção básica de aparelho conectado

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import subprocess
 
 
 try:
-    result = subprocess.run(["adb", "--version"])
-    if result.returncode == 0:
-        print("ADB disponivel!")
+    resultado = subprocess.run(
+        ["adb", "devices"],
+        capture_output=True,
+        text=True
+    ) 
+    if resultado.returncode == 0:
+        linhas = resultado.stdout.strip().splitlines()
+        if len(linhas) > 1:
+            print(linhas[1])
+        else:
+            print("Nenhum aparelho conectado!")
     else:
         print("ADB retornou um erro!")
 except FileNotFoundError:
     print("ADB não encontrado!")
-    
