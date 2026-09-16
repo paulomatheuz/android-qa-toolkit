54f5e658eda606f480fedbff188b500a80b3169f
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-16 19:35:05 -0400

    Adiciona consulta à versão do Android com tratamento de falha

--- main.py
+++ main.py
@@ -16,29 +16,42 @@ try:
             print(f"Numero de serie: {numero_serie}")
             if estado == "device":
                 print("Aparelho conectado e autorizado!")
+
+
                 resultado_fabricante = subprocess.run(
                     ["adb", "shell", "getprop", "ro.product.manufacturer"],
                     capture_output= True,
                     text=True
                 )
-
                 if resultado_fabricante.returncode == 0:
                     fabricante = resultado_fabricante.stdout.strip()
                     print(f"Fabricante: {fabricante}")
                 else:
                     print("Não foi possível consultar o fabricante!")
 
+
                 resultado_modelo = subprocess.run(
                     ["adb", "shell", "getprop", "ro.product.model"],
                     capture_output=True,
                     text=True
                 )
-
                 if resultado_modelo.returncode == 0:
                     modelo = resultado_modelo.stdout.strip()
                     print(f"Modelo: {modelo}")
                 else:
                     print("Não foi possível consultar o modelo!")
+
+
+                resultado_versao_android = subprocess.run(
+                    ["adb", "shell", "getprop", "ro.build.version.release"],
+                    capture_output=True,
+                    text=True
+                )
+                if resultado_versao_android.returncode == 0:
+                    versao_android = resultado_versao_android.stdout.strip()
+                    print(f"Versão Android: {versao_android}")
+                else:
+                    print("Não foi possível consultar a versão do android!")
             else:
                 print(f"Estado do aparelho: {estado}")
         else:
